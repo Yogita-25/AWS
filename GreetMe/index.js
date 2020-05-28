484aa8e291ac54d8eced3c51088ef04113d13bac
Yogita.Dhanwate <Yogita.Dhanwate@harbingergroup.com>
2020-05-28 19:43:27 +0530

    understanding of context object

--- GreetMe/index.js
+++ GreetMe/index.js
@@ -1,13 +1,15 @@
-exports.handler = async (event, context) => {
-    const data = event.data;
-    let newImage = await resizeImage();
-    return newImage;
+exports.handler = async(event,context)=>{
+    context.getRemainingTimeInMillis();
+
+    context.functionName;   //gives name of the lambda function
+    context.functionVersion;
+    context.functionArn;
+    context.awsRequestId;
+    context.memoryLimitInMB;
+    context.identity;       //gives information about congnito identity provider
+    context.logGroupName;
+    context.logStreamName;
+    context.clientContext;    //gives additional information about client device
 }
 
-const resizeImage = (data) => new Promise((resolve, reject) => {
-    if (error) {
-        reject(error);
-    } else {
-        resolve(result);
-    }
-});
+
